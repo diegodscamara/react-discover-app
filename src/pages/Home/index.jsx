602b696709f98648edc839184bfb2a021ed86729
Diego Câmara <diego.camara@nsh.com.br>
2022-05-29 16:35:34 -0300

    New students list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,19 +4,34 @@ import { Card } from "../../components/Card";
 
 export function Home() {
   const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
+    setStudents((prevState) => [...prevState, newStudent]);
+  }
 
   return (
     <div className="container">
-      <h1>Nome: {studentName}</h1>
+      <h1>Lista de presença</h1>
       <input
         type="text"
         placeholder="Digite o nome..."
         onChange={(e) => setStudentName(e.target.value)}
       />
-      <button type="button">Adicionar</button>
-      <Card name="Diego" time="10:55:25" />
-      <Card name="Vanessa" time="11:10:26" />
-      <Card name="Maria" time="12:16:55" />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+      {students.map((student) => (
+        <Card key={student.id} name={student.name} time={student.time} />
+      ))}
     </div>
   );
 }
