84686a52a8554359fc50645615c01107599ce712
Diego Câmara <diego.camara@nsh.com.br>
2022-05-29 11:44:37 -0300

    Create a state to store data

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 
-export function Card({name, time}) {
+export function Card({ name, time }) {
+
   return (
     <div className="card">
       <strong>{name}</strong>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,18 @@
+import React, { useState } from "react";
 import "./styles.css";
 import { Card } from "../../components/Card";
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+
   return (
     <div className="container">
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..."></input>
+      <h1>Nome: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
       <Card name="Diego" time="10:55:25" />
       <Card name="Vanessa" time="11:10:26" />
